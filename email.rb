535d2a1f84b80e77f2cd51edf3233a6a8cd3c1a6
Roman <roman.g.rodriguez@gmail.com>
2013-06-23 00:33:18 -0300

    Update email.rb

--- email.rb
+++ email.rb
@@ -1,82 +1,86 @@
 class Email
 
-	include Gmail
+  include Gmail
 
-	FILENAME = 'email_config.yaml'
+  FILENAME = 'email_config.yaml'
 
-	def initialize(user=nil, password=nil)
-   	if user.nil? and password.nil?
-   		data = YAML::load(File.open(FILENAME))
-   		user=data['email']
-   		password=data['password']
-   	end
-   	begin
- 			@gmail = Gmail.connect!(user,password)
- 		rescue Net::IMAP::BadResponseError
- 			puts "CANNOT LOGIN TO GMAIL WITH GIVEN CREDENTIALS"
- 		end
- 	end
+  def initialize(user=nil, password=nil)
+    if user.nil? and password.nil?
+      data = YAML::load(File.open(FILENAME))
+      user=data['email']
+      password=data['password']
+      @folder=data['folder']
+    end
+    begin
+      @gmail = Gmail.connect!(user,password)
+    rescue Net::IMAP::BadResponseError
+      puts "CANNOT LOGIN TO GMAIL WITH GIVEN CREDENTIALS"
+    end
+  end
 
- 	def connected?
-   	@gmail.logged_in?    
- 	end
+  def connected?
+    @gmail.logged_in?    
+  end
 
- 	def logout
-   	@gmail.logout
+  def logout
+    @gmail.logout
   end
 
   def last_inbox_email
-  	@gmail.inbox.mails.last
+    @gmail.inbox.mails.last
   end
 
   def last_inbox_unread_email
-  	@gmail.inbox.find(:unread).last
+    @gmail.inbox.find(:unread).last
   end	
 
   def last_email_from_label(label_name)
-  	@gmail.label(label_name).emails.last
+    @gmail.label(label_name).emails.last
   end
 
   def last_email_from(from)
-  	@gmail.inbox.find(:from => from).last
+    @gmail.inbox.find(:from => from).last
   end
 
   def mark_as_unread mail
-  	mail.unread!
+    mail.unread!
   end
 
   def mark_as_read mail
-  	mail.read!
+    mail.read!
   end
 
   def mark_as_starred mail
-  	mail.star!
+    mail.star!
   end
 
   def mark_as_unstarred mail
-  	mail.unstar!
+    mail.unstar!
   end
 
   def download_attachment mail
+    if !mail.message.attachments.empty?
+      mail.message.save_attachments_to(@folder)
+    end
   end
 
   def delete_all_emails_with_label(label)
-   	@gmail.emails(:label => label).each do |email|
-     	email.delete!
-   	end
- 	end
+    @gmail.emails(:label => label).each do |email|
+      email.delete!
+    end
+  end
 
-  	def emails_with_subject(subject)
-    	@gmail.emails(:subject => label)
- 		end
+  def emails_with_subject(subject)
+    @gmail.emails(:subject => label)
+  end
 
- 		def send_email(email_to,email_subject,email_body)
-    	email = @gmail.compose do
-      	to email_to
-      	subject email_subject
-      	body email_body
-    	end
-    	email.deliver!
-  	end
+  def send_email(email_to,email_subject,email_body)
+    email = @gmail.compose do
+      to email_to
+      subject email_subject
+      body email_body
+    end
+    email.deliver!
+  end
 
-	end	
+end	
